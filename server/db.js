1eb3fe236efc58fed5109497081b59e8575f4332
Young <youngheesomemoto@gmail.com>
2024-03-15 21:25:07 -0500

    unsure how favorties is working, and how the constraint is working

--- server/db.js
+++ server/db.js
@@ -1,9 +1,100 @@
 // client - a node pg client
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_notes_categories_db');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+
 // createTables method - drops and creates the tables for your application
+const createTables = async () => {
+    const SQL = `
+      DROP TABLE IF EXISTS favorites;
+      DROP TABLE IF EXISTS users;
+      DROP TABLE IF EXISTS products;
+      CREATE TABLE users(
+        id UUID DEFAULT gen_random_uuid(),
+        username VARCHAR(20) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
+      );
+      CREATE TABLE products(
+        id UUID DEFAULT gen_random_uuid(),
+        name VARCHAR(20),
+        PRIMARY KEY (id)
+      );
+      CREATE TABLE favorites(
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        user_id UUID REFERENCES users(id) NOT NULL,
+        product_id UUID REFERENCES products(id) NOT NULL,
+        CONSTRAINT unique_user_id_and_product_id UNIQUE (user_id, product_id)
+      );
+    `;
+    await client.query(SQL);
+};
 // createProduct - creates a product in the database and returns the created record
+const createProducts = async ({ name }) => {
+    const SQL = `
+      INSERT INTO products(id, name) VALUES($1, $2) RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), name]);
+    return response.rows[0];
+};
 // createUser - creates a user in the database and returns the created record. The password of the user should be hashed using bcrypt.
+const createUser = async ({ username, password }) => {
+    const SQL = `
+      INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), username, await bcrypt.hash(password, 5)]);
+    return response.rows[0];
+};
 // fetchUsers - returns an array of users in the database
+const fetchUsers = async () => {
+    const SQL = `
+      SELECT id, username FROM users;
+    `;
+    const response = await client.query(SQL);
+    return response.rows;
+};
 // fetchProducts - returns an array of products in the database
+const fetchProducts = async () => {
+    const SQL = `
+      SELECT * FROM products;
+    `;
+    const response = await client.query(SQL);
+    return response.rows;
+};
 // fetchFavorites - returns an array favorites for a user
+const fetchFavorites = async (user_id) => {
+    const SQL = `
+      SELECT * FROM favorites where user_id = $1
+    `;
+    const response = await client.query(SQL, [user_id]);
+    return response.rows;
+};
 // createFavorite - creates a favorite in the database and returns the created record
+const createFavorite = async ({ user_id, product_id }) => {
+    const SQL = `
+      INSERT INTO favorites(id, user_id, product_id) VALUES($1, $2, $3) RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), user_id, product_id]);
+    return response.rows[0];
+};
 // destroyFavorite - deletes a favorite in the database
+const destroyFavorite = async ({ user_id, id }) => {
+    const SQL = `
+      DELETE FROM favorites WHERE user_id=$1 AND id=$2
+    `;
+    await client.query(SQL, [user_id, id]);
+};
+
+
+module.exports = {
+    client,
+    createTables,
+    createFavorite,
+    createProducts,
+    createUser,
+    fetchFavorites,
+    fetchUsers,
+    fetchProducts,
+    destroyFavorite
+}
--- server/index.js
+++ server/index.js
@@ -1,42 +1,101 @@
-//middleware
 const {
-    client, 
-    createTable,
-    createFavorites,
+    client,
+    createTables,
+    createFavorite,
     createProducts,
+    createUser,
     fetchFavorites,
     fetchUsers,
-    fetchProducts 
-  } = require('./db');
-  const express = require('express');
-  const app = express();
-  app.use(express.json());
+    fetchProducts,
+    destroyFavorite
+} = require('./db');
+const express = require('express');
+const app = express();
+app.use(express.json());
 
 // GET /api/users - returns array of users
-app.get('/api/users', async(req, res, next)=> {
-    try{
+app.get('/api/users', async (req, res, next) => {
+    try {
         res.send(await fetchUsers());
-    } catch(ex){
-        next(ex)
+    }
+    catch (ex) {
+        next(ex);
     }
 });
 // GET /api/products - returns an array of products
-app.get('/api/products', async(req, res, next) => {
-    try{
-        res.send(await fetchProducts());
-    } catch(ex) {
+app.get('/api/products', async (req, res, next) => {
+    try {
+        res.send(fetchProducts());
+    }
+    catch (ex) {
         next(ex);
     }
 });
 // GET /api/users/:id/favorites - returns an array of favorites for a user
-app.get('/api/users/:id/favorites', async(req, res, next) => {
-    try{
+app.get('/api/users/:id/favorites', async (req, res, next) => {
+    try {
         res.send(await fetchFavorites());
-    } catch(ex) {
+    }
+    catch (ex) {
         next(ex);
     }
 });
-// POST /api/users/:id/favorites - payload: a product_id
-app.post('/api/users/:id/favorites', async())
-// returns the created favorite with a status code of 201
+// POST /api/users/:id/favorites - payload: a product_id returns the created favorite with a status code of 201
+app.post('/api/users/:id/favorites', async (req, res, next) => {
+    try {
+        res.status(201).send(await createFavorite({ user_id: req.params.id, product_id: req.body.product_id }));
+    }
+    catch (ex) {
+        next(ex);
+    }
+});
 // DELETE /api/users/:userId/favorites/:id - deletes a favorite for a user, returns nothing with a status code of 204
+app.delete('/api/users/:user_id/favorites/:id', async (req, res, next) => {
+    try {
+        await destroyFavorite({ user_id: req.params.user_id, id: req.params.id });
+        res.sendStatus(204);
+    }
+    catch (ex) {
+        next(ex);
+    }
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send({ error: err.message ? err.message : err });
+});
+
+const init = async () => {
+    const port = process.env.PORT || 3000;
+    await client.connect();
+    console.log('connected to database');
+
+    await createTables();
+    console.log('tables created');
+
+    const [moe, lucy, ethyl, curly, foo, bar, bazz, quq, fip] = await Promise.all([
+        createUser({ username: 'moe', password: 'm_pw' }),
+        createUser({ username: 'lucy', password: 'l_pw' }),
+        createUser({ username: 'ethyl', password: 'e_pw' }),
+        createUser({ username: 'curly', password: 'c_pw' }),
+        createProducts({ name: 'foo' }),
+        createProducts({ name: 'bar' }),
+        createProducts({ name: 'bazz' }),
+        createProducts({ name: 'quq' }),
+        createProducts({ name: 'fip' })
+    ]);
+    const favorites = await Promise.all([
+        createFavorite({ user_id: moe.id, product_id: foo.id }),
+        createFavorite({ user_id: moe.id, product_id: fip.id }),
+        createFavorite({ user_id: ethyl.id, product_id: bazz.id})
+    ])
+    console.log("users", await fetchUsers());
+    console.log("products", await fetchProducts());
+    console.log("favorites", await fetchFavorites(moe.id, ethyl.id));
+
+    console.log(`moe's id is ${moe.id}`);
+
+    app.listen(port, () => console.log(`listening on port ${port}`));
+};
+
+init();
